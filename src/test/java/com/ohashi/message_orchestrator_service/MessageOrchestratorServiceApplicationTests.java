9b4a88c3280a1a64986725df41f5f8fc1fdb08f0
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-06-01 02:13:14 -0300

    first commit

---
+++ src/test/java/com/ohashi/message_orchestrator_service/MessageOrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.message_orchestrator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageOrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
