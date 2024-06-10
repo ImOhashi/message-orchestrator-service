fc730d1c01991a0fcae92f26dbf3b82f4677c3a0
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-06-09 23:53:49 -0300

    feat: add a custom kafka listener annotation

---
+++ src/main/java/com/ohashi/message_orchestrator_service/application/listeners/annotations/DefaultListener.java
@@ -0,0 +1,21 @@
+package com.ohashi.message_orchestrator_service.application.listeners.annotations;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@KafkaListener
+public @interface DefaultListener {
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "topics")
+    String[] topics() default "message-orchestrator-topic";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
+    String groupId() default "";
+}
